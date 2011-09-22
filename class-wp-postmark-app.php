979971b1ae554ca22a50537da799d31ed9051b35
Sean O'Shaughnessy <sean@voceconnect.com>
2011-09-22 10:52:49 -0400

    instead subclass phpmailer and override the send method... fallback to just sending an email via phpmailer if postmark constants aren't defined...

---
+++ class-wp-postmark-app.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * subclass of PHPMailer that instead uses the Postmark API
+ * to send e-mail.
+ *
+ * @package WP Postmark App
+ **/
+class WPPostmarkApp extends PHPMailer {
+	public function __construct() {
+		//include the postmark api...
+		include(__DIR__."/postmark-php/Postmark.php");
+	}
+
+	/**
+	 * hooks into phpmailer_init and gets the phpmailer object.
+	 * we'll pull the data we need from the object, then update the object's
+	 * "to:" address to be null, thus causing $phpmailer->send() to fail
+	 * this way we don't send the same message twice - once through postmark,
+	 * and again through phpmailer.
+	 *
+	 * @return (boolean) - whether or not sending via postmark api failed...
+	 **/
+	public function Send() {
+		// make sure our constants have been defined...
+		if( !defined('POSTMARKAPP_API_KEY') || !defined('POSTMARKAPP_MAIL_FROM_ADDRESS') || !defined('POSTMARKAPP_MAIL_FROM_NAME')) {
+			// ...if nothing is defined, fall back to parent class Send method
+			return parent::Send();
+		}
+
+		// use PHP5 Reflection to get the private "$to" var out of
+		// the phpmailer object...
+		// http://www.php.net/manual/en/book.reflection.php
+		$ref_class = new ReflectionClass('PHPMailer');
+		$ref_property = $ref_class->getProperty('to');
+		$ref_property->setAccessible(true);
+		$phpmailers_to = $ref_property->getValue($this);
+
+		// i miss teh nitrous
+		$phpmailers_to = $phpmailers_to[0];
+
+		// remove empty elements in the array
+		foreach ($phpmailers_to as $k => $v) {
+			if ($v == "")
+				unset($phpmailers_to[$k]);
+		}
+
+		// set a comma separated string of to addresses
+		$postmark_to = implode(',', $phpmailers_to);
+
+		// set up the other vars we need
+		$postmark_subject = $this->Subject;
+		$postmark_message_plain = $this->Body;
+
+		// set up the postmark mail object
+		$postmark_email = new Mail_Postmark();
+		$postmark_email->addTo($postmark_to);
+		$postmark_email->subject($postmark_subject);
+		$postmark_email->messagePlain($postmark_message_plain);
+
+		// send it!
+		try {
+			$postmark_email->send();
+			return true;
+		} catch (Exception $e) {
+			$this->SetError($e->getMessage());
+			if ($this->exceptions) {
+				throw $e;
+			}
+			return false;
+		}
+	}
+}
--- wp-postmark-app.php
+++ wp-postmark-app.php
@@ -8,72 +8,23 @@ Author: Sean O'Shaughnessy, with a major assist from Mike Pretty who put up with
 Author URI: http://voceconnect.com
 */
 
-// include the Postmark API
-// taken from https://github.com/Znarkus/postmark-php
-include(dirname(__FILE_)."/postmark-php/Postmark.php");
+// require PHP 5
+if (version_compare(PHP_VERSION, '5.0.0', '<') ) exit("Sorry, WP Postmark App will only run on PHP version 5 or greater!\n");
 
 /**
- * hooks into phpmailer_init and gets the phpmailer object.
- * we'll pull the data we need from the object, then update the object's
- * "to:" address to be null, thus causing $phpmailer->send() to fail
- * this way we don't send the same message twice - once through postmark,
- * and again through phpmailer.
+ * set the global phpmailer object to our WP Postmark subclass...
  *
  * @return void
  **/
-function postmarkapp_mailer($phpmailer) {
-	// make sure our constants have been defined, otherwise return
-	if( !defined('POSTMARKAPP_API_KEY') || !defined('POSTMARKAPP_MAIL_FROM_ADDRESS') || !defined('POSTMARKAPP_MAIL_FROM_NAME'))
-		die("DEFINE YOUR STUFF"); #return;
+function extend_phpmailer_with_wppostmarkapp() {
+	global $phpmailer;
 
-	// use PHP5 Reflection to get the private "$to" var out of
-	// the phpmailer object...
-	// http://www.php.net/manual/en/book.reflection.php
-	$ref_class = new ReflectionClass('PHPMailer');
-	$ref_property = $ref_class->getProperty('to');
-	$ref_property->setAccessible(true);
-	$phpmailers_to = $ref_property->getValue($phpmailer);
-
-	// i miss teh nitrous
-	$phpmailers_to = $phpmailers_to[0];
-
-	// remove empty elements in the array
-	foreach ($phpmailers_to as $k => $v) {
-		if ($v == "")
-			unset($phpmailers_to[$k]);
+	if ( !is_object( $phpmailer ) || !is_a( $phpmailer, 'PHPMailer' ) ) {
+		require_once ABSPATH . WPINC . '/class-phpmailer.php';
+		require_once ABSPATH . WPINC . '/class-smtp.php';
 	}
 
-	// set a comma separated string of to addresses
-	$postmark_to = implode(',', $phpmailers_to);
-
-	// set up the other vars we need
-	$postmark_subject = $phpmailer->Subject;
-	$postmark_message_plain = $phpmailer->Body;
-
-	// set up the postmark mail object
-	$postmark_email = new Mail_Postmark();
-	$postmark_email->addTo($postmark_to);
-	$postmark_email->subject($postmark_subject);
-	$postmark_email->messagePlain($postmark_message_plain);
-
-	// send it!
-	try {
-		$postmark_email->send();
-	} catch (Exception $e) {
-		error_log($e);
-	}
-
-	// clear $phpmailer object to address
-	$phpmailer->ClearAddresses();
-	$phpmailer->ClearAllRecipients();
-	$phpmailer->ClearBCCs();
-	$phpmailer->ClearCCs();
-	$phpmailer->ClearCustomHeaders();
-
-	// use reflection again to set exceptions to false
-	// on phpmailer, this way it "appears" it was sent
-	$ref_property = $ref_class->getProperty('exceptions');
-	$ref_property->setAccessible(true);
-	$ref_property->setValue($phpmailer, false);
+	include(__DIR__.'/class-wp-postmark-app.php');
+	$phpmailer = new WPPostmarkApp();
 }
-add_action('phpmailer_init', 'postmarkapp_mailer');
+add_action('init', 'extend_phpmailer_with_wppostmarkapp');
